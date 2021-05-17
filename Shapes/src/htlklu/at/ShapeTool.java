655fd73d86177ac5b33dc9c37f389d6292d1f4f1
Samuel Mancini (3BHEL) <samuel.mancini@htl-klu.at>
2021-05-17 09:12:40 +0200

    added methode in ShapeTool class

--- Shapes/src/htlklu/at/ShapeTool.java
+++ Shapes/src/htlklu/at/ShapeTool.java
@@ -1,5 +1,7 @@
 package htlklu.at;
 
+import java.awt.Color;
+import java.awt.Point;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,8 +11,24 @@ import java.util.List;
 
 public class ShapeTool {
 	public static void main(String[] args) {
-		List <Shape> shapes = generateRandomShapes(9);
+		List<Shape> shapes = generateRandomShapes(9);
 		writeShapesToFile(shapes, "sorted_shapes.txt");
+	}
+
+	public static void setPositionsAndColors(List<Shape> shapes, int maxX, int maxY) {
+		for (Shape s : shapes) {
+			final int x = (int) Math.random() * maxX;
+			final int y = (int) Math.random() * maxY;
+
+			s.setPoint(new Point(x, y));
+			if (s instanceof Circle) {
+				s.setColor(Color.BLUE);
+			} else if (s instanceof Rectangle) {
+				s.setColor(Color.RED);
+			} else if (s instanceof Square) {
+				s.setColor(Color.GREEN);
+			}
+		}
 	}
 
 	public static void writeShapesToFile(List<Shape> shapes, String filename) {
@@ -18,11 +36,11 @@ public class ShapeTool {
 			FileWriter fw = new FileWriter(filename);
 			BufferedWriter bw = new BufferedWriter(fw);
 			Collections.sort(shapes);
-			for(Shape s : shapes) {
+			for (Shape s : shapes) {
 				bw.write(s.toString() + "\n");
 			}
 			bw.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
